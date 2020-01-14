3f3c1f1b3e6935afe907abc453f3a86eae6a784f
Anantvir Singh <anantvir91@gmail.com>
2020-01-14 12:14:22 -0500

    Is valid parenthesis

--- Array_Manipulations/Valid_Parenthesis.py
+++ Array_Manipulations/Valid_Parenthesis.py
@@ -5,12 +5,12 @@ def IsValid(arr):
     closing_dict = {")":"(","]":"[","}":"{"}
     stack =[]
     for i in range(len(arr)):
-        if arr[i] in closing_dict:
+        if arr[i] in closing_dict:  # check if its a closing bracket
             p = stack.pop()
-            if p!= closing_dict[arr[i]]:
+            if p!= closing_dict[arr[i]]:  # if closing bracket then compare top element of stack with opening bracket from closing_dict  
                 return False
         else:
-            stack.append(arr[i])
+            stack.append(arr[i])        # push opening brackets onto stack
     return True
 
 print(IsValid(list('([()])')))
