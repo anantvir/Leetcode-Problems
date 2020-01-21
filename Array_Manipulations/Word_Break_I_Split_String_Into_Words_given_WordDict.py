099b4e177922f04010768f67e803c6757059c5c5
Anantvir Singh <anantvir91@gmail.com>
2020-01-21 18:03:43 -0500

    Word_Break_I_Split_String_into_Words_given_WordDict

---
+++ Array_Manipulations/Word_Break_I_Split_String_Into_Words_given_WordDict.py
@@ -0,0 +1,52 @@
+
+
+
+wordDict = ["leet","code","singh","anantvir"]
+s = "leetcodesinghanantvir"
+
+
+"""------------------ Inefficient Approach 1 --------------------"""
+def Wordbreak(s,start):
+    if start == len(s):
+        return True
+    for end in range(start+1,len(s)):
+        if s[start:end+1] in wordDict and Wordbreak(s,end+1):
+            return True
+    return False
+
+"""------------------ Inefficient Approach 2 --------------------"""
+def Wordbreak2(s):
+    if len(s) == 0:
+        return True
+    for i in range(len(s)):
+        for j in range(len(s)):
+            if s[i:j+1] in wordDict:
+                s_new = s[:i] + s[j+1:]
+                if Wordbreak2(s_new) == True:
+                    return True
+    return False
+
+"""--------------- Dynamic Programming Approach 3------------------"""
+"""Here is for example s = "catsanddogs" then we split it into "catsand" & "dogs". Now both of these strings must
+satisfy the constraints i.e they must be optimal substructures. So make an array dp[n] where n represents the 
+length of string which can be broken down into dictionary words. dp[0]=True because string of length 0 is always
+a member of word dict. Take a pointer i from 1 to len(s). i starts from 1 because i represents length of the string being considered
+currently and for i=0 we have already filled the array dp[0]=True. The second pointer j splits the string
+of length i into two substrings s1 and s2 such that both of them must also be optimal. We check is s1 is optimal
+from the array dp, and check if s2 exists in the given wordDict. If yes then we set dp[i] = True
+
+https://leetcode.com/problems/word-break/solution/
+"""
+def Wordbreak3(s):
+    dp = [False]*(len(s)+1)
+    dp[0] = True                # String of length 0 can always be broken into worddict words i.e its always part of worddict
+    for i in range(1,len(s)+1):   # i = 1 to len(s) example is s="applepenapple" then i = 1 to 13 where index of last e = 12
+        for j in range(0,i):    # j = 0 to i-1
+            if dp[j] == True and s[j:i] in wordDict:
+                dp[i] = True
+                break
+    return dp[len(s)]
+
+
+#print(Wordbreak(s,0))
+print(Wordbreak3(s))
