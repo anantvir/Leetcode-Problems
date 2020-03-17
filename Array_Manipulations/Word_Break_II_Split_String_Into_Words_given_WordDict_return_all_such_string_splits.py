0b9f09884039fea8a702e494b9921df38a196eb7
Anantvir Singh <anantvir91@gmail.com>
2020-03-16 21:58:22 -0400

    Word Break II

--- Array_Manipulations/Word_Break_II_Split_String_Into_Words_given_WordDict_return_all_such_string_splits.py
+++ Array_Manipulations/Word_Break_II_Split_String_Into_Words_given_WordDict_return_all_such_string_splits.py
@@ -1,8 +1,40 @@
 
+"""----------------------- Dynamic Programming using array -------------------------"""
+"""dp[k] stores a list of valid strings which can be formed by string of length k taken from the input string.
+Example for string "catsanddog", dp[7] stores all the valid strings that can be formed until length 7 of the input string
+i.e dp[7] = ["cat sand","cats and"]"""
+class Solution(object):
+    def wordBreak(self, s, wordDict):
+        """
+        :type s: str
+        :type wordDict: List[str]
+        :rtype: List[str]
+        """
+        hashset = set()
+        for word in wordDict:
+            hashset.add(word)
+        dp = [0]*(len(s)+1)
+        init = ['']
+        dp[0] = init
+        for i in range(1,len(s)+1):
+            curr_lst = []
+            for j in range(0,i):
+                print(len(dp[j]))
+                if len(dp[j]) > 0 and s[j:i] in hashset:
+                    for eachStr in dp[j]:
+                        if eachStr == '':
+                            curr_lst.append(eachStr + s[j:i])
+                        else:
+                            curr_lst.append(eachStr + ' ' + s[j:i])
+            dp[i] = curr_lst
+        return dp[len(s)]
 
+wordDict = ["cat", "cats", "and", "sand", "dog"]
+st = "catsanddog"
 
-wordDict = ["apple","pen","apple"]
-s = "applepenapple"
+s = Solution()
+print(s.wordBreak(st,wordDict))
+
 
 def WordBreak_II(s,start):
     res = []
@@ -20,7 +52,4 @@ def WordBreak_II(s,start):
                 else:
                     space = " "
                 res.append(temp+space+string)
-
     return res
-
-print(WordBreak_II(s,0))
